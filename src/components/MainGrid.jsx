2f65b83bbbb5acc2970d2d748f85cc69154b3726
bellhwi <rlawhdgnl1@naver.com>
2022-11-02 20:29:24 -0700

    Change image file format

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -5,25 +5,25 @@ function MainGrid({ setModalOn }) {
     <div className='mt-10 mx-auto grid grid-cols-1 sm:grid-cols-2 items-center gap-4 w-10/12 lg:absolute lg:right-0 lg:mr-8 pb-10'>
       <Thumbnail
         setModalOn={setModalOn}
-        url={require('../images/haxa.png')}
+        url={require('../images/haxa.webp')}
         title={'Haxa'}
         id={0}
       />
       <Thumbnail
         setModalOn={setModalOn}
-        url={require('../images/pokemon-guesser.png')}
+        url={require('../images/pokemon-guesser.webp')}
         title={'Pokemon Guesser'}
         id={1}
       />
       <Thumbnail
         setModalOn={setModalOn}
-        url={require('../images/todoro.png')}
+        url={require('../images/todoro.webp')}
         title={'Todoro'}
         id={2}
       />
       <Thumbnail
         setModalOn={setModalOn}
-        url={require('../images/arnid.png')}
+        url={require('../images/arnid.webp')}
         title={'Arnid Xiong'}
         id={3}
       />
--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 const data = [
   {
-    url: require('./images/haxa2.png'),
+    url: require('./images/haxa2.webp'),
     title: 'Haxa',
     desc: 'This is a commercial products website that is outsourced from my client.',
     github: 'https://github.com/bellhwi/haxa',
     website: 'https://haxa-16f57.web.app/',
   },
   {
-    url: require('./images/pokemon-guesser2.png'),
+    url: require('./images/pokemon-guesser2.webp'),
     title: 'Pokemon Guesser',
     desc: "This is an API project that guesses what a user's Pokemon is.",
     github: 'https://github.com/bellhwi/pokemon-guesser',
     website: 'https://pokemon-guesser.netlify.app/',
   },
   {
-    url: require('./images/todoro2.png'),
+    url: require('./images/todoro2.webp'),
     title: 'Todoro',
     desc: 'This is a minimal todo list project with a progress bar .',
     github: 'https://github.com/bellhwi/todoro',
     website: 'https://todoro-react.netlify.app/',
   },
   {
-    url: require('./images/arnid2.png'),
+    url: require('./images/arnid2.webp'),
     title: 'Arnid Xiong',
     desc: 'This is a graphic design portfolio website for one of my clients.',
     github: 'https://github.com/bellhwi/ArnidXiong',
